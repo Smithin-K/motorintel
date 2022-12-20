552174dc69f298f526993db0280648cc5f4ae5ef
Your Name <you@example.com>
2022-12-20 16:57:56 +0530

    updates

--- scrape_redbook.py
+++ scrape_redbook.py
@@ -1,3 +1,4 @@
+import requests
 from bs4 import BeautifulSoup
 from constants import DATA_FOLDER, ROOT_DIR
 
@@ -8,10 +9,15 @@ class ScrapeRedbook:
     def __init__(self):
         self.base_url = "https://www.redbook.com.au/cars/results?q=%28Make%3D%5B{}%5D%26Model%3D%5B{}%5D%29"
 
-    def fetch_html(self, file_name='redbook_home.html'):
-        with open(f"{ROOT_DIR}/{DATA_FOLDER}/{file_name}") as fp:
-            file = fp.read()
-            return file
+    # def fetch_html(self, file_name='redbook_home.html'):
+    #     with open(f"{ROOT_DIR}/{DATA_FOLDER}/{file_name}") as fp:
+    #         file = fp.read()
+    #         return file
+
+    def html_fetch_scraperapi(self):
+        payload = {'api_key': '5f92293ed53821a76f06e39f7afafc21', 'url': 'https://httpbin.org/ip'}
+        r = requests.get('http://api.scraperapi.com', params=payload)
+
 
 
     def find_div(self, html, div_class):
